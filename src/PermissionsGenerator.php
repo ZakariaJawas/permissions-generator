7a5e7e3037038a24805e1df8e398a82e73c5bbbc
Zakaria Jawas <jawas.zakaria@gmail.com>
2022-11-08 03:02:45 +0300

    docs

--- src/PermissionsGenerator.php
+++ src/PermissionsGenerator.php
@@ -11,33 +11,43 @@ use Illuminate\Support\Facades\Schema;
 
 class PermissionsGenerator {
 
+    /**
+     * Generate permissions for your project models
+     */
     public function generate() {
 
+        //check if permissions table exists
         if (!Schema::hasTable('permissions')) {
 
             return "Please install spatie permissions package first, and run php artisan migrate then run permissions:generate command";
         } //end if
-        //get models
+
+        //get project models
         $models = $this->getModels();
 
-        //check if models exists
+        //check if project conains any models
         if (count($models) == 0) {
 
             return "You don't have any models yet, create models then run the command again\n";            
         } //end if
         
         
-        //process models 
+        //process models => extract models names and convert it it permission style name
         $processedModels = $this->processModels($models);
-
+        
         foreach($processedModels as $model) {
 
             $this->generatePermissions($model);
         } //end for each
-        return "Success\n";
+
+        return "Permissions are generated, check your table.\n";
     }
 
 
+    /**
+     * Get a collection of the project models
+     * @return Collection
+     */
     private function getModels(): Collection
     {
         
@@ -68,21 +78,24 @@ class PermissionsGenerator {
     }
 
     /**
-     * Used to convert models name into permissions names
+     * Used to convert models name into permissions style names
      * Rules:-
      * Lowercased
      * Compund Names are Separated with Spaces
+     * 
+     * @param Collection $models
+     * @return Array
      */
     private function processModels(Collection $models) {
 
         $result = [];
         foreach ($models as $model) {
 
-            //extract model class name
+            //extract model name from class path
             $modelSections = explode('\\', "$model");
             $modelName = end($modelSections);
 
-            //split Pascal model name into array of names            
+            //split Pascal model name into array of names          
             $matches =  preg_split('/(?=[A-Z]{1}[a-z]+)/', $modelName, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
 
             $name = strtolower(implode(' ',  $matches));
@@ -91,9 +104,16 @@ class PermissionsGenerator {
         return $result;
     }
 
+    /**
+     * Used to generate the permissions for a model and insert it to database
+     * @param string $model
+     */
     private function generatePermissions($model) {
 
+        //get prefix value from config file
         $prefix = Config::get("permissionsgenerator.prefix");        
+
+        //get permissions values from config file
         $permissions = Config::get("permissionsgenerator.permissions");
 
         $query = [];
